c87852038325a0e37c83c64ac4ee83004f32fd69
DualScorch <jakob.tormalm@telia.com>
2014-10-26 21:40:18 +0100

    OreDictRecipes

--- src/main/java/com/scorch/testmod/init/Recipes.java
+++ src/main/java/com/scorch/testmod/init/Recipes.java
@@ -5,12 +5,15 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class Recipes
 {
     public static void init()
     {
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.tree),"ddd", "lll", "ddd", 'd', new ItemStack(Items.diamond), 'l', new ItemStack(Blocks.log));
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.leaf), new ItemStack(ModBlocks.tree), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves));
+
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.tree),"ddd", "lll", "ddd", 'd', "gemDiamond", 'l', "logWood"));
+        GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.leaf), new ItemStack(ModBlocks.tree), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves)));
     }
 }
