e4e03daa7dc5d2a8f64689f4b7e4142d8958a7bb
DualScorch <jakob.tormalm@telia.com>
2014-10-26 20:47:07 +0100

    New Better Recipes

--- src/main/java/com/scorch/testmod/init/Recipes.java
+++ src/main/java/com/scorch/testmod/init/Recipes.java
@@ -10,7 +10,7 @@ public class Recipes
 {
     public static void init()
     {
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.tree),"ddd", "   ", "ddd", 'd', new ItemStack(Items.diamond), 'l', new ItemStack(Blocks.wooden_slab));
-        GameRegistry.addRecipe(new ItemStack(ModItems.leaf), "lll", "lwl", "lll", 'l', new ItemStack(Blocks.leaves), 'w', new ItemStack(ModBlocks.tree));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.tree),"ddd", "lll", "ddd", 'd', new ItemStack(Items.diamond), 'l', new ItemStack(Blocks.log));
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.leaf), new ItemStack(ModBlocks.tree), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves), new ItemStack(Blocks.leaves));
     }
 }
